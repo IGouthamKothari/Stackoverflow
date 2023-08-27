e68a611e7e8a83ef278c13968438a81cdec310f0
Goutham B <110347988+IGouthamKothari@users.noreply.github.com>
2023-08-27 06:22:17 +0530

    Initial commit

---
+++ src/Pages/Auth/Auth.jsx
@@ -0,0 +1,140 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+import "./Auth.css";
+import icon from "../../assets/icon.png";
+import AboutAuth from "./AboutAuth";
+import { signup, login } from "../../actions/auth";
+
+const Auth = () => {
+  const [isSignup, setIsSignup] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleSwitch = () => {
+    setIsSignup(!isSignup);
+    setName("");
+    setEmail("");
+    setPassword("");
+  };
+
+  const validateEmail = (email) => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,2,3}\.[0-9]{1,2,3}\.[0-9]{1,2,3}\.[0-9]{1,2,3}])|(([a-zA-Z\-0-9]+.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!email || !password) {
+      alert("Enter email and password");
+      return;
+    }
+    if (!validateEmail(email)) {
+      alert("Enter a valid email address");
+      return;
+    }
+    if (password.length < 8) {
+      alert("Password must be at least 8 characters long");
+      return;
+    }
+    if (!/\d/.test(password)) {
+      alert("Password must contain at least one numeric digit");
+      return;
+    }
+    if (!/[a-zA-Z]/.test(password)) {
+      alert("Password must contain at least one letter");
+      return;
+    }
+    if (!/[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]+/.test(password)) {
+      alert("Password must contain at least one special character");
+      return;
+    }
+    
+    if (isSignup) {
+      if (!name) {
+        alert("Enter a name to continue");
+        return;
+      }
+      dispatch(signup({ name, email, password }, navigate));
+    } else {
+      dispatch(login({ email, password }, navigate));
+    }
+  };
+  
+
+  return (
+    <section className="auth-section">
+      {isSignup && <AboutAuth />}
+      <div className="auth-container-2">
+        <img src={icon} alt="stack overflow" className="login-logo" />
+        <form onSubmit={handleSubmit}>
+          {isSignup && (
+            <label htmlFor="name">
+              <h4>Display Name</h4>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={name}
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
+            </label>
+          )}
+          <label htmlFor="email">
+            <h4>Email</h4>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+          </label>
+          <label htmlFor="password">
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <h4>Password</h4>
+              {!isSignup && (
+                <p style={{ color: "#007ac6", fontSize: "13px" }}>
+                  forgot password?
+                </p>
+              )}
+            </div>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </label>
+          <button type="submit" className="auth-btn">
+            {isSignup ? "Sign up" : "Log in"}
+          </button>
+        </form>
+        <p>
+          {isSignup ? "Already have an account?" : "Don't have an account?"}
+          <button
+            type="button"
+            className="handle-switch-btn"
+            onClick={handleSwitch}
+          >
+            {isSignup ? "Log in" : "sign up"}
+          </button>
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Auth;
